ad1140d3007b2a8ebe078d7316bca9c2cb8d34e4
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-08-20 21:38:23 +0100

    FEAT

--- main.go
+++ main.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	_ "image/png"
 	"os"
 
 )
+
 func RGBToGraycale(r uint32, g uint32,b uint32) float32{
 	result := float32(r)*0.299 + float32(g)*0.587 + float32(b)*0.114
 	return result
 }
+func pixelToChar(gray uint8) rune{
+	
+	const asciiRamp = "@%#*+=-:. "
+	scale := float32(gray) /255
+
+	index := int(scale * float32(len(asciiRamp)-1))
+	return rune(asciiRamp[index])
+
+}
 func main(){
-	file,err := os.Open("dd.jpg")
+	file,err := os.Open("images/smol.png")
 	if err != nil {
-		panic("could not open file")
+		panic("Could not open file")
 	}
 	defer file.Close()
 	img,_,err := image.Decode(file)
@@ -28,13 +39,16 @@ func main(){
 	for i := 0; i!= height; i++{
 		grayScaleMatrix[i] = make([]uint8,width)
 	}
-	for i := 0 ;i!= height; i++ {
-		
-		for j := 0; j!= height; j++ {
-			r,g,b,_ := img.At(i,j).RGBA()
-			grayScaleMatrix[i][j] = uint8(RGBToGraycale(r,g,b))
-			fmt.Printf("%3d ",grayScaleMatrix[i][j] )
-		}
-		fmt.Println()
-	}
-}
+for y := 0; y < height; y++ {
+    for x := 0; x < width; x++ {
+        r, g, b, _ := img.At(x, y).RGBA()
+        r8 := uint8(r >> 8)
+        g8 := uint8(g >> 8)
+        b8 := uint8(b >> 8)
+
+        gray := uint8(RGBToGraycale(uint32(r8), uint32(g8), uint32(b8)))
+        char := pixelToChar(gray)
+        fmt.Printf("%c", char)
+    }
+    fmt.Println()
+}}
